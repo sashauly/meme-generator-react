fd55bf5e63d4c21db7580fbff1c8bb574cc61133
stonehom <stonehom@student.21-school.ru>
2023-06-27 21:57:59 +0300

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="./images/troll-face.png" alt="" className="header__logo" />
+      <h2 className="header__title">Meme Generator</h2>
+      <h3 className="header__course">React Course - Project 3</h3>
+    </header>
+  );
+}
