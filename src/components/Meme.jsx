1a5623ded8549ac2d009f956777580f022ca75c4
stonehom <stonehom@student.21-school.ru>
2023-06-27 22:23:12 +0300

    fix meme form

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,13 +3,11 @@ import React from "react";
 export default function Meme() {
   return (
     <main>
-      <form action="#" className="form">
+      <div className="form">
         <input type="text" className="form__input" placeholder="Top text" />
         <input type="text" className="form__input" placeholder="Bottom text" />
-        <button type="submit" className="form__button">
-          Get a new meme image 🖼
-        </button>
-      </form>
+        <button className="form__button">Get a new meme image 🖼</button>
+      </div>
     </main>
   );
 }
