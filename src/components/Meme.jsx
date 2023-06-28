32bfaba74ee69fcfcb235fc12d228f779658b4e4
stonehom <stonehom@student.21-school.ru>
2023-06-28 11:14:09 +0300

    add random meme image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,30 +1,24 @@
 import React from "react";
 import memesData from "../memesData.js";
 
-/**
- * Challenge: Get a random image from the `memesData` array
- * when the "new meme image" button is clicked.
- *
- * Log the URL of the image to the console. (Don't worry
- * about displaying the image yet)
- */
 export default function Meme() {
-  function handleClick() {
+  const [memeImage, setMemeImage] = React.useState("");
+  function getMemeImage() {
     const memesArray = memesData.data.memes;
     const randomElement =
       memesArray[Math.floor(Math.random() * memesArray.length)];
-    const url = memesArray[randomElement].url;
-    console.log(url);
+    setMemeImage(randomElement.url);
   }
   return (
     <main>
       <div className="form">
         <input type="text" className="form__input" placeholder="Top text" />
         <input type="text" className="form__input" placeholder="Bottom text" />
-        <button className="form__button" onClick={handleClick}>
+        <button className="form__button" onClick={getMemeImage}>
           Get a new meme image 🖼
         </button>
       </div>
+      <img src={memeImage} alt="" className="meme__image" />
     </main>
   );
 }
