90e831a53c835a5de8bfdab7f5f41d35baffc22b
stonehom <stonehom@student.21-school.ru>
2023-06-30 19:57:44 +0300

    add async await parameters

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -10,11 +10,11 @@ export default function Meme() {
   const [allMemes, setAllMemes] = React.useState([]);
 
   React.useEffect(() => {
-    fetch(`https://api.imgflip.com/get_memes`)
-      .then((res) => res.json())
-      .then((data) => {
-        setAllMemes(data.data.memes);
-      });
+    (async () => {
+      const res = await fetch(`https://api.imgflip.com/get_memes`);
+      const data = await res.json();
+      setAllMemes(data.data.memes);
+    })();
   }, []);
 
   function getMemeImage() {
