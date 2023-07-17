5ef3ca01c858530ed1854a29c5050ab72dffb853
sashauly <sashauly.code@gmail.com>
2023-07-17 13:43:29 +0300

    remove h3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
-import React from "react";
+import React from 'react';
 
 export default function Header() {
   return (
     <header className="header">
       <img src="./images/troll-face.png" alt="" className="header__logo" />
       <h2 className="header__title">Meme Generator</h2>
-      <h3 className="header__course">React Course - Project 3</h3>
     </header>
   );
 }
